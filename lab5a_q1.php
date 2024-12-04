849a700c910efd9bad0232682e8015c5e4c0f40d
sharkdgaf <nsijaja30@gmail.com>
2024-12-05 02:43:00 +0800

    Add files via upload

---
+++ lab5a_q1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+</head>
+<body>
+    <?php 
+        $name = "Nur Shakirah I'zzah Binti Zamri";
+        $matric_number = "AI220191";
+        $course = "BIS";
+        $year_of_study = "3";
+        $address = "Taman Desa Ros, 70400 Seremban, Negeri Sembilan";
+    ?>
+
+    <table border="1">
+        <tr>
+            <td>Name</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>Matric Number</td>
+            <td><?php echo $matric_number; ?></td>
+        </tr>
+        <tr>
+            <td>Course</td>
+            <td><?php echo $course; ?></td>
+        </tr>
+        <tr>
+            <td>Year of Study</td>
+            <td><?php echo $year_of_study; ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo $address; ?></td>
+        </tr>
+    </table>
+    
+</body>
+</html>
